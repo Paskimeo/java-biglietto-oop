8b483d24f38ccd00b393216364cd845080b7d529
paskimeo <paskimeo@gmail.com>
2022-06-17 14:34:14 +0200

    aggiunto attributi e costruttore

--- src/biglietto/controller/biglietto.java
+++ src/biglietto/controller/biglietto.java
@@ -2,4 +2,17 @@ package biglietto.controller;
 
 public class biglietto {
 
+	//attributi
+	private double prezzoKm = 0.21, scontoAnziani = 0.4, scontoGiovani = 0.2;
+	private int eta,km;
+	
+	
+	//costruttore
+	public biglietto(int eta, int km) {
+		super();
+		this.eta = eta;
+		this.km = km;
+	}
+
+	
 }
